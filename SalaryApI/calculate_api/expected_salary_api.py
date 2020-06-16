2a40ba72af585e39d86c3c88cf68aa2863552822
Ebonee Gilford <55034390+Qweeyn7@users.noreply.github.com>
2020-06-15 17:27:26 -0700

    Intro to API

---
+++ SalaryApI/calculate_api/expected_salary_api.py
@@ -0,0 +1,69 @@
+from flask import Flask  # import Flask
+from flask import render_template
+from flask import request
+
+from calculate_api.utilities import calculator
+
+app = Flask(__name__)  # activate the Flask application/ instantiate the Flask object
+
+
+@app.route('/hello_world')
+def hello_world():
+    return "Hello, Tribe!"
+
+
+@app.route('/')
+def candidate_form():
+    """
+
+    :return:
+    """
+    return render_template('index.html')
+
+
+@app.route('/calculate_salary', methods=['POST'])
+def calculate_salary():
+    if request.method == "POST":
+        candidate_type = request.form["profession"]
+        number_of_exp_years = int(request.form["experience"])
+        user_coding_languages = request.form['languages']
+        user_trade_tools = user_coding_languages.split(",")
+        user_design_tools = request.form['designTools']
+        user_trade_tools = user_design_tools.split(",")
+        dob = request.form['dob']
+        fullName = request.form['fullName']
+        age = int(request.form['age'])
+        country = request.form['country']
+        state = request.form['state']
+        number_of_education_years = int(request.form['educationYears'])
+
+    candidate_developer = False
+    candidate_designer = False
+
+    if int(candidate_type) == 1:
+        candidate_developer = True
+        candidate_designer = False
+    elif int(candidate_type) == 2:
+        candidate_developer = False
+        candidate_designer = True
+    else:
+        return "OK"
+
+    user_profile = candidate_developer(dob, age, fullName, country, state,
+                                       number_of_education_years, user_trade_tools)
+
+    user_profile = candidate_designer(dob, age, fullName, country, state,
+                                      number_of_education_years, user_trade_tools)
+
+    candidate_type_map = {
+        "1": "developer",
+        "2": "designer"
+    }
+
+    candidate_type_str = candidate_type_map[candidate_type]
+
+    calculator.expected_salary = calculator.calculate_expected_salary_from_user_experience(user_profile,
+                                                                                           user_trade_tools,
+                                                                                           number_of_exp_years,
+                                                                                           number_of_education_years,
+                                                                                           candidate_type_str)
---
+++ SalaryApI/calculate_api/utilities/calculator.py
@@ -0,0 +1,175 @@
+expected_salaries = {"NY": 70000, "CA": 70000, "FL": 50000, "NC": 50000, "TX": 60000}
+expected_salary = 0
+
+
+def calculate_expected_salary_from_user_experience(user_information, user_trade_tools,
+                                                   user_experience, number_of_education_years, candidate_type_str):
+    """
+      A function for calculating the expected salary based on the user's
+      state, and their years of experience.
+  """
+
+    # Get the user's state from the incoming user_information object
+    state = user_information["State"]
+
+    # Set the base salary, based on the user's state
+    base_salary = expected_salaries[state]
+
+    # Initialize a new_expected_salary variable, which is set to the base_salary
+    new_expected_salary = base_salary
+
+    if isinstance(user_information, dict):
+        return "All Fields have been completed."
+        print()
+    else:
+        return "This is not a dictionary. It is {}".format(type(user_information))
+        print()
+
+    # Re-calculate the salary based on the user's experience.
+    if int(user_experience) == 1:
+        new_expected_salary = base_salary - 5000
+        return "Unfortunately with your limited experience we will have to deduct $5k from your base salary."
+    elif int(user_experience) == 2:
+        new_expected_salary = base_salary + 2000
+        return "With your level of experience, expect a $2k bump in your base salary."
+    elif int(user_experience) == 3:
+        new_expected_salary = base_salary + 5000
+        return "Exactly what we are looking for! Expect a $5K added to your base salary."
+    elif int(user_experience) == 4:
+        new_expected_salary = base_salary + 10000
+        return (
+            "We're giving you an additional $10k increase to your base salary for the many years of experience you "
+            "have "
+            "in this field.")
+
+    candidate_developer = False
+    candidate_designer = False
+
+    candidate_type_wording_map = {
+        "developer": "coding languages",
+        "designer": "design programs"
+    }
+
+    candidate_type_wording = candidate_type_wording_map[candidate_type_str]
+
+    if len(user_trade_tools) < 3:
+        new_expected_salary = new_expected_salary - 7500
+        print("Learn more " + candidate_type_wording + ": $7.5K was deducted from the expected salary.")
+    elif len(user_trade_tools) > 3:
+        new_expected_salary = new_expected_salary + 10000
+        print("Learning more than 3 " + candidate_type_wording + " added $10K to expected salary.")
+    else:
+        new_expected_salary = new_expected_salary + 5000
+        print("Knowing 3 " + candidate_type_wording + " or less only added $5K to expected salary.")
+    if int(number_of_education_years) > 3:
+        new_expected_salary = new_expected_salary + 10000
+        return "Dedication to learning added $10K to base salary "
+    elif int(number_of_education_years) < 3:
+        new_expected_salary = new_expected_salary - 7500
+        return "Number for years of education needs improvement: Deduct $7.5K from base salary."
+    else:
+        new_expected_salary = new_expected_salary - 2000
+        return "Continue learning to increase salary: added $2K to expected salary."
+    return "Expect around $" + str(new_expected_salary) + " annually for this position."
+    print()
+    return new_expected_salary
+
+
+current_salary = 0
+
+
+def calculate_expected_salary_from_coding_experience(current_salary, user_languages, user_experience, user_trade_tools):
+    # Re-calculate the salary, based off the user experience, and the number of languages they know.
+    new_expected_salary = current_salary
+    if int(user_experience) == 1:
+        if len(user_languages) < 2:
+            new_expected_salary = new_expected_salary - 10000
+            return "Learn more Coding Languages: Deduct $10k from expected salary."
+        elif len(user_languages) == 2:
+            new_expected_salary = new_expected_salary + 5000
+        else:
+            new_expected_salary = new_expected_salary + 2500
+    if int(user_experience) == 2:
+        if len(user_languages) == 2:
+            new_expected_salary = new_expected_salary - 5000
+        elif len(user_languages) == 3:
+            new_expected_salary = new_expected_salary + 7000
+        else:
+            new_expected_salary = new_expected_salary + 4500
+    if len(user_languages) == 3:
+        new_expected_salary = new_expected_salary + 7000
+        return "Great! We can add a minimum of $7k to your annual salary for the amount of Coding Languages you know."
+    if int(user_experience) == 3:
+        if len(user_languages) < 3:
+            new_expected_salary = new_expected_salary - 5000
+        elif len(user_languages) == 4:
+            new_expected_salary = new_expected_salary + 9000
+        else:
+            new_expected_salary = new_expected_salary + 6500
+    if len(user_languages) == 4:
+        new_expected_salary = new_expected_salary + 9000
+        return (
+            "Perfect! You are a model candidate for the position, we can negotiate a $9k bump in you annual salary "
+            "after "
+            "for the amount of Coding Languages you know.")
+    if int(user_experience) == 4:
+        if len(user_languages) == 4:
+            new_expected_salary = new_expected_salary + 9000
+        elif len(user_languages) == 5:
+            new_expected_salary = new_expected_salary + 11000
+        else:
+            new_expected_salary = new_expected_salary + 8500
+    if len(user_languages) == 5:
+        new_expected_salary = new_expected_salary + 11000
+        return (
+            "With your proficiency with multiple Coding Languages, we can negotiate a $11k bump in your annual salary.")
+    else:
+        candidate_designer = True
+        new_expected_salary = current_salary
+
+    if int(user_experience) == 1:
+        if len(user_trade_tools) < 2:
+            new_expected_salary = new_expected_salary - 10000
+            return "Learn more Coding Languages: Deduct $10k from expected salary."
+        elif len(user_trade_tools) == 2:
+            new_expected_salary = new_expected_salary + 5000
+        else:
+            new_expected_salary = new_expected_salary + 2500
+        if int(user_experience) == 2:
+            if len(user_trade_tools) == 2:
+                new_expected_salary = new_expected_salary - 5000
+            elif len(user_trade_tools) == 3:
+                new_expected_salary = new_expected_salary + 7000
+            else:
+                new_expected_salary = new_expected_salary + 4500
+        if len(user_trade_tools) == 3:
+            new_expected_salary = new_expected_salary + 7000
+            return (
+                "Great! We can add a minimum of $7k to your annual salary for the amount of Coding Languages you know.")
+        if int(user_experience) == 3:
+            if len(user_trade_tools) < 3:
+                new_expected_salary = new_expected_salary - 5000
+            elif len(user_trade_tools) == 4:
+                new_expected_salary = new_expected_salary + 9000
+            else:
+                new_expected_salary = new_expected_salary + 6500
+        if len(user_trade_tools) == 4:
+            new_expected_salary = new_expected_salary + 9000
+            return (
+                "Perfect! You are a model candidate for the position, we can negotiate a $9k bump in you annual "
+                "salary after "
+                "for the amount of Coding Languages you know. ")
+        if int(user_experience) == 4:
+            if len(user_trade_tools) == 4:
+                new_expected_salary = new_expected_salary + 9000
+            elif len(user_trade_tools) == 5:
+                new_expected_salary = new_expected_salary + 11000
+            else:
+                new_expected_salary = new_expected_salary + 8500
+        if len(user_trade_tools) == 5:
+            new_expected_salary = new_expected_salary + 11000
+            return (
+                "With your proficiency with multiple Coding Languages, we can negotiate a $11k bump in your annual "
+                "salary.")
+
+    return new_expected_salary
