cf8f43d3e1e6dc5dc5490cdfbd8a69a2d400ca03
Ebonee Gilford <55034390+Qweeyn7@users.noreply.github.com>
2020-06-22 16:13:54 -0700

    Included projects 2 & 3 Voice to text and text to voice.

--- TicTac/HappyDayCard.py
+++ TicTac/HappyDayCard.py
@@ -1,35 +1,41 @@
-"""
-from asciimatics.effects import Cycle, Stars
+# 1st project Happy Father's Day
+"""from asciimatics.effects import Cycle, Stars
 from asciimatics.renderers import FigletText
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
-
-
 def demo(screen):
     effects = {
         Cycle(
             screen,
             FigletText("Happy", font='big'),
-            int(screen.height / 2 - 8)),
+            int(screen.height / 2 - 9)),
         Cycle(
             screen,
             FigletText("Father's    Day!", font='big'),
-            int(screen.height / 2 + 3)),
-        Stars(screen, 200)
+            int(screen.height / 4 + 5)),
+        Stars(screen, 400)
     }
-    screen.play([Scene(effects, 500)])
+    screen.play([Scene(effects, 700)])
 
 
 Screen.wrapper(demo)
 """
-#Father's Day Card has multiple errors.
 
-import speech_recognition as sr
-
+# 2nd project voice-to-text
+"""import speech_recognition as sr
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Speak Now: ")
     audio = r.listen(source)
 print(r.recognize_google(audio))
+"""
 
+# 3rd project text-to-voice
+"""import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello I'm Jade. Okay. I love pancakes.")
+engine.runAndWait()
+"""
+
+# 4th project play music using python
 
