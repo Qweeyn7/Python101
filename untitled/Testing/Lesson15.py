850bdf8c8b114fe8e55f33b77f5d4c71fda50d5c
Ebonee Gilford <55034390+Qweeyn7@users.noreply.github.com>
2020-08-30 23:34:41 -0700

    Python is finished. Updated AD.html from output.timeofappointment to output.time Changed return resuls to return output Upgraded my index file

---
+++ untitled/Testing/Lesson15.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from datetime import datetime
+from Testing.utilities import scheduler
+
+days_of_week = {
+    "1": "Sunday",
+    "2": "Monday",
+    "3": "Tuesday",
+    "4": "Wednesday",
+    "5": "Thursday",
+    "6": "Friday",
+    "7": "Saturday",
+}
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def appointment_form():
+    return render_template("index.html")
+
+
+@app.route('/schedule_app', methods=['POST'])
+def schedule_app():
+    if request.method == 'POST':
+        numeric_day_of_appointment = request.form["day_appt"]
+        time_of_appointment = request.form['time_appt']
+        service_type = request.form['serviceType']
+        day_of_appointment = days_of_week[numeric_day_of_appointment]
+        if day_of_appointment == "Sunday":
+            return render_template("WeAreClosed.html")
+        else:
+            scheduler.calculate_services = scheduler.calculate_services_price(day_of_appointment,
+                                                                              time_of_appointment,
+                                                                              service_type)
+
+        return render_template("appointmentDetails.html",
+                               output=scheduler.calculate_services)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
